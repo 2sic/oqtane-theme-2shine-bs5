892a5fd8135ab3075a6bd1e245779f8c01b5b78d
2dar <daia.robbi@2sic.com>
2022-03-18 10:18:43 +0100

    some changes to the folder and file structure

--- Client/Default/Default.razor.cs
+++ Client/Default/Default.razor.cs
@@ -8,135 +8,140 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Net.Http;
 
-namespace ToSic.Oqt.Themes.ToShineBs5.Client.Default
+namespace ToSic.Oqt.Themes.ToShineBs5.Client.Default;
+
+public partial class Default : Oqtane.Themes.ThemeBase
 {
-    public partial class Default : Oqtane.Themes.ThemeBase
+    public override string Name => "Default";
+
+    public override List<Resource> Resources => new()
     {
-        protected virtual string ClassName => "default";
+        // Load Theme which contains Bootstrap with our customizations (generated with Sass using Webpack)
+        new Resource { ResourceType = ResourceType.Stylesheet, Url = ToShineThemePath() + "theme.min.css" },
+        // Load the default BS JS
+        new Resource { ResourceType = ResourceType.Script, Url = ToShineThemePath() + "bootstrap.bundle.min.js" },
+        // Our custom code which ensures page classes, Up-button etc.
+        new Resource { ResourceType = ResourceType.Script, Url = ToShineThemePath() + "ambient.js" },
+    };
 
-        protected virtual bool ShowSidebarNavigation => false;
+    public override string Panes => PaneNames.Admin + ",Header,Content";
 
-        protected virtual bool ShowBreadcrumb => true;
+    //Set 2shine variables to define some settings for this layout
+    //------------------------------------------------------------
 
-        public static string ToShineThemePath()
+    //ClassName variable is used to set the body class which sets the css for this specific layout
+    protected virtual string ClassName => "default";
+
+    protected virtual bool ShowSidebarNavigation => false;
+
+    protected virtual bool ShowBreadcrumb => true;
+
+
+    [Inject]
+    protected HttpClient Http { get; set; }
+
+    [Inject]
+    protected IPageService PageService { get; set; }
+
+    [Inject]
+    protected ISiteService SiteService { get; set; }
+
+    [Inject]
+    protected ITenantService TenantService { get; set; }
+
+    [Inject]
+    protected ILanguageService LanguageService { get; set; }
+
+
+    public static string ToShineThemePath()
+    {
+        string path = "Themes/ToSic.Oqt.Themes.ToShineBs5/";
+        return path;
+    }
+
+    private IJSObjectReference BodyClassJS;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        string bodyClasses = await DetermineBodyClasses();
+
+        BodyClassJS = await JSRuntime.InvokeAsync<IJSObjectReference>("import", Path.Combine("./", ToShineThemePath() ,"interop/page-control.js"));
+
+        await BodyClassJS.InvokeAsync<string>("clearBodyClasses");
+        await BodyClassJS.InvokeAsync<string>("setBodyClass", bodyClasses);
+    }
+
+    private async Task<string> DetermineBodyClasses()
+    {
+        var page = this.PageState.Page;
+
+        //1.1 Set the page-is-home class
+        var isHomeClass = page.Path == "" ? "page-is-home" : "";
+        
+        //1.2 Set the page-### class
+        var pageIdClass = "page-" + page.PageId;
+
+        //1.3 Set the page-parent-### class
+        var pageParentClass = page.ParentId == null ? null : "page-parent-" + page.ParentId;
+
+        //1.4 Set the page-root-### class
+        var pageParentId = page.ParentId;
+        string pageRootClass = null;
+        if (pageParentId == null)
         {
-            string path = "Themes/ToSic.Oqt.Themes.ToShineBs5/";
-            return path;
+            pageRootClass = "page-root-" + page.PageId;
         }
-
-        public override string Name => "default";
-
-        public override string Panes => PaneNames.Admin + ",Header,Content";
-
-        public override List<Resource> Resources => new()
+        else
         {
-		    // Load Theme which contains Bootstrap with our customizations (generated with Sass using Webpack)
-            new Resource { ResourceType = ResourceType.Stylesheet, Url = ToShineThemePath() + "theme.min.css" },
-            // Load the default BS JS
-            new Resource { ResourceType = ResourceType.Script, Url = ToShineThemePath() + "bootstrap.bundle.min.js" },
-            // Our custom code which ensures page classes, Up-button etc.
-            new Resource { ResourceType = ResourceType.Script, Url = ToShineThemePath() + "ambient.js" },
-        };
-
-        [Inject]
-        protected HttpClient Http { get; set; }
-
-        [Inject]
-        protected IPageService PageService { get; set; }
-
-        [Inject]
-        protected ISiteService SiteService { get; set; }
-
-        [Inject]
-        protected ITenantService TenantService { get; set; }
-
-        [Inject]
-        protected ILanguageService LanguageService { get; set; }
-
-        private IJSObjectReference BodyClassJS;
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            await base.OnAfterRenderAsync(firstRender);
-
-            string bodyClasses = await DetermineBodyClasses();
-
-            BodyClassJS = await JSRuntime.InvokeAsync<IJSObjectReference>("import", Path.Combine("./", ToShineThemePath() ,"interop/page-control.js"));
-
-            await BodyClassJS.InvokeAsync<string>("clearBodyClasses");
-            await BodyClassJS.InvokeAsync<string>("setBodyClass", bodyClasses);
-        }
-
-        private async Task<string> DetermineBodyClasses()
-        {
-            var page = this.PageState.Page;
-
-            //1.1 Set the page-is-home class
-            var isHomeClass = page.Path == "" ? "page-is-home" : "";
-            
-            //1.2 Set the page-### class
-            var pageIdClass = "page-" + page.PageId;
-
-            //1.3 Set the page-parent-### class
-            var pageParentClass = page.ParentId == null ? null : "page-parent-" + page.ParentId;
-
-            //1.4 Set the page-root-### class
-            var pageParentId = page.ParentId;
-            string pageRootClass = null;
-            if (pageParentId == null)
+            while (pageParentId != null)
             {
-                pageRootClass = "page-root-" + page.PageId;
-            }
-            else
-            {
-                while (pageParentId != null)
+                var parentPage = await PageService.GetPageAsync((int)pageParentId);
+                pageParentId = parentPage.ParentId;
+                if (parentPage.ParentId == null)
                 {
-                    var parentPage = await PageService.GetPageAsync((int)pageParentId);
-                    pageParentId = parentPage.ParentId;
-                    if (parentPage.ParentId == null)
-                    {
-                        pageRootClass = "page-root-" + parentPage.PageId;
-                    }
+                    pageRootClass = "page-root-" + parentPage.PageId;
                 }
             }
-
-            //1.5 Set the page-root-neutral-### class
-
-            //2 Set site-### class
-            var siteIdClass = "site-" + page.SiteId;
-
-            //3 Set the nav-level-### class
-            var navLevel = page.Level + 1;
-            var navLevelClass = "nav-level-" + navLevel;
-
-            //4.1 Set lang- class
-
-            //4.2 Set the lang-root- class
-
-            //4.3 Set the lang-neutral- class
-
-            //5.1 Set the to-shine-variation- class
-            var layoutVarriationClass = "to-shine-variation-" + ClassName;
-
-            //5.2 Set the to-shine-mainnav-variation- class
-            var navigationVariationClass = "to-shine-mainnav-variation-right";
-
-            string[] classes =
-            {
-                pageIdClass,
-                pageParentClass,
-                pageRootClass,
-                isHomeClass,
-                siteIdClass,
-                navLevelClass,
-                layoutVarriationClass,
-                navigationVariationClass,
-            };
-
-            string bodyClasses = string.Join(" ", classes);
-
-            bodyClasses = bodyClasses.Replace("  ", " ");
-            return bodyClasses;
         }
+
+        //1.5 Set the page-root-neutral-### class
+
+        //2 Set site-### class
+        var siteIdClass = "site-" + page.SiteId;
+
+        //3 Set the nav-level-### class
+        var navLevel = page.Level + 1;
+        var navLevelClass = "nav-level-" + navLevel;
+
+        //4.1 Set lang- class
+
+        //4.2 Set the lang-root- class
+
+        //4.3 Set the lang-neutral- class
+
+        //5.1 Set the to-shine-variation- class
+        var layoutVarriationClass = "to-shine-variation-" + ClassName;
+
+        //5.2 Set the to-shine-mainnav-variation- class
+        var navigationVariationClass = "to-shine-mainnav-variation-right";
+
+        string[] classes =
+        {
+            pageIdClass,
+            pageParentClass,
+            pageRootClass,
+            isHomeClass,
+            siteIdClass,
+            navLevelClass,
+            layoutVarriationClass,
+            navigationVariationClass,
+        };
+
+        string bodyClasses = string.Join(" ", classes);
+
+        bodyClasses = bodyClasses.Replace("  ", " ");
+        return bodyClasses;
     }
 }
