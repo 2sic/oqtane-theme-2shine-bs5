fa35f043e0844e3a90a48ec17c1d3bfcb166d599
getBlup <daia.robbi@gmail.com>
2022-05-20 12:46:18 +0200

    added theme settings

---
+++ Client/ThemeSettings/ThemeDisplaySettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToSic.Oqt.Themes.ToShineBs5.Client.ThemeSettings
+{
+    public partial class ThemeDisplaySettings
+    {
+        public string StartingPage = null;
+
+        public int? StartLevel = null;
+
+        public List<int> PageList;
+
+        public int LevelSkip;
+
+        public int LevelDepth;
+
+        public bool Display;
+
+        public string Variation;
+    }
+}
---
+++ Client/ThemeSettings/ThemeSettingsService.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace ToSic.Oqt.Themes.ToShineBs5.Client.ThemeSettings
+{
+    public sealed class ThemeSettingsService
+    {
+        Dictionary<string, ThemeDisplaySettings> CombinedSettings;
+
+        private ThemeDisplaySettings Settings;
+
+        private IJSObjectReference BodyClassJS;
+
+        public Dictionary<string, ThemeDisplaySettings> DeserializeData()
+        {
+            string jsonString = File.ReadAllText("./settings.json");
+            return CombinedSettings = JsonSerializer.Deserialize<Dictionary<string, ThemeDisplaySettings>>(jsonString);
+        }
+
+        public void SerializeDataAsync(string ConfigName, ThemeDisplaySettings Settings)
+        {
+            CombinedSettings.Add(ConfigName, Settings);
+
+            string jsonString = JsonSerializer.Serialize(CombinedSettings);
+            File.WriteAllText("./settings.json", jsonString);
+        }
+    }
+}
