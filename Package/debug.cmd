2e958b6c4b1837fa7d5f6992f135f8a8bc8dd5b0
Marc Drexel <marc.drexel@2sic.com>
2022-06-01 15:28:56 +0200

    removed Package project, not needed with dotnet pack

--- Package/debug.cmd
+++
@@ -1,4 +0,0 @@
-@REM XCOPY "..\Client\bin\Debug\net6.0\ToSic.Oqt.Themes.ToShineBs5.Client.Oqtane.dll" "..\..\website\bin\Debug\net6.0\" /Y
-@REM XCOPY "..\Client\bin\Debug\net6.0\ToSic.Oqt.Themes.ToShineBs5.Client.Oqtane.pdb" "..\..\website\bin\Debug\net6.0\" /Y
-
-XCOPY "..\Client\dist\wwwroot\*" "..\..\website\wwwroot\" /Y /S /I
--- Package/release.cmd
+++
@@ -1,4 +0,0 @@
-@REM TODO: NUGET.exe sollte über Standard-Pfad für Nuget CLI aufgerufen werden (evtl. im build process abrufen/installieren?) 
-@REM "..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Oqt.Themes.ToShineBs5.nuspec 
-
- XCOPY "*.nupkg" "..\..\website\wwwroot\Themes\" /Y
