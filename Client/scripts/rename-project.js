d9194a342602ba3288c769491fcc8b1ec04d0176
Marc Drexel <marc.drexel@2sic.com>
2022-06-03 12:37:11 +0200

    rename script

--- Client/scripts/rename-project.js
+++ Client/scripts/rename-project.js
@@ -1,40 +1,58 @@
-const replace = require("replace-in-file");
-const shell = require("shelljs");
-const path = require("path");
 const fs = require("fs");
 
-if (!fs.existsSync("ToSic.Oqt.Themes.ToShineBs5.Client.csproj")) {
-  console.log(
-    "it appear you already renamed your theme, this can only be done once!"
-  );
-} else {
-  const prompt = require("prompt");
-  const properties = [
-    {
-      description: "theme name (namespace eg. ToSic.Oqt.Themes.MyFancyTheme)",
-      name: "newThemeName",
-      required: true,
-      validator: /^(@?[a-z_A-Z]\w+(?:\.@?[a-z_A-Z]\w+)*)+$/,
-      warning: "your theme name must be a valid c# namespace",
-    },
-  ];
+if (!fs.existsSync("node_modules")) {
+  const npm = require("npm");
+  npm.load(function (err) {
+    npm.on("log", function (message) {
+      console.log(message);
+    });
 
-  prompt.start();
-  prompt.get(properties, function (err, result) {
-    if (err) {
-      return onErr(err);
-    }
-
-    renameTheme(result.newThemeName);
+    npm.commands.ci([], function (er, data) {
+      promptForRename();
+    });
   });
+} else {
+  promptForRename();
+}
 
-  function onErr(err) {
-    console.log(err);
-    return 1;
+function promptForRename() {
+  const fs = require("fs");
+
+  if (!fs.existsSync("ToSic.Oqt.Themes.ToShineBs5.Client.csproj")) {
+    console.log(
+      "it appear you already renamed your theme, this can only be done once!"
+    );
+  } else {
+    const prompt = require("prompt");
+    const properties = [
+      {
+        description: "theme name (namespace eg. ToSic.Oqt.Themes.MyFancyTheme)",
+        name: "newThemeName",
+        required: true,
+        validator: /^(@?[a-z_A-Z]\w+(?:\.@?[a-z_A-Z]\w+)*)+$/,
+        warning: "your theme name must be a valid c# namespace",
+      },
+    ];
+
+    prompt.start();
+    prompt.get(properties, function (err, result) {
+      if (err) {
+        return onErr(err);
+      }
+
+      renameTheme(result.newThemeName);
+    });
+
+    function onErr(err) {
+      console.log(err);
+      return 1;
+    }
   }
 }
 
 function renameTheme(newThemeName) {
+  const replace = require("replace-in-file");
+
   const defaultThemeName = "ToSic.Oqt.Themes.ToShineBs5";
   if (newThemeName === defaultThemeName) {
     console.log(
