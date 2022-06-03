d5f1449c428b6cd51b42348a10e0f8b1a01c7be4
Marc Drexel <marc.drexel@2sic.com>
2022-06-03 11:50:26 +0200

    rename script

--- Client/scripts/rename-project.js
+++ Client/scripts/rename-project.js
@@ -4,36 +4,61 @@ const shell = require("shelljs");
 const path = require("path");
 const fs = require("fs");
 
-const defaultThemeName = "ToSic.Oqt.Themes.ToShineBs5";
+const prompt = require("prompt");
+const properties = [
+  {
+    description: "Theme-Name (namespace eg. ToSic.Oqt.Themes.MyFancyTheme)",
+    name: "newThemeName",
+    required: true,
+    validator: /^(@?[a-z_A-Z]\w+(?:\.@?[a-z_A-Z]\w+)*)+$/,
+    warning: "your theme name must be a valid c# namespace",
+  },
+];
 
-let themeConfig = require("../theme.jsonc");
-if (!themeConfig || !themeConfig.ThemeName) {
-  themeConfig = {
-    ThemeName: "ToSic.Oqt.Themes.ToShineBs5",
-  };
+prompt.start();
+prompt.get(properties, function (err, result) {
+  if (err) {
+    return onErr(err);
+  }
+
+  renameTheme(result.newThemeName);
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
 }
 
-if (themeConfig.ThemeName === defaultThemeName) {
-} else {
-  const options = {
-    files: ["**/*.razor", "**/*.cs", "**/*.csproj", "**/*.sln"],
-    ignore: [
-      "obj/**",
-      "bin/**",
-      "node_modules/**",
-      "scripts/**",
-      "dist/**",
-      ".temp_cache/**",
-      ".vscode/**",
-    ],
-    from: RegExp(defaultThemeName, "g"),
-    to: themeConfig.ThemeName,
-  };
-
-  try {
-    const results = replace.sync(options);
-    console.log("Replacement results:", results);
-  } catch (error) {
-    console.error("Error occurred:", error);
+function renameTheme(newThemeName) {
+  const defaultThemeName = "ToSic.Oqt.Themes.ToShineBs5";
+  if (newThemeName === defaultThemeName) {
+    console.log("Theme already renamed");
+  } else {
+    const options = {
+      files: ["**/*.razor", "**/*.cs", "**/*.csproj", "../*.sln"],
+      ignore: [
+        "obj/**",
+        "bin/**",
+        "node_modules/**",
+        "scripts/**",
+        "dist/**",
+        ".temp_cache/**",
+        ".vscode/**",
+      ],
+      from: RegExp(defaultThemeName, "g"),
+      to: newThemeName,
+    };
+    try {
+      const results = replace.sync(options);
+      console.log("Replacement results:", results);
+      const defaultCsharpProject = "ToSic.Oqt.Themes.ToShineBs5.Client.csproj";
+      const defaultVsSolution = "../ToSic.Oqt.Themes.ToShineBs5.sln";
+      if (fs.existsSync(defaultCsharpProject))
+        fs.renameSync(defaultCsharpProject, `${newThemeName}.Client.csproj`);
+      if (fs.existsSync(defaultVsSolution))
+        fs.renameSync(defaultVsSolution, `../${newThemeName}.sln`);
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
   }
 }
