146114afb72bec9c02319613335d6b86bbd900fd
Marc Drexel <marc.drexel@2sic.com>
2022-06-03 12:46:20 +0200

    naming fixes

--- Client/Layouts/ThemeInfo.cs
+++ Client/Layouts/ThemeInfo.cs
@@ -11,7 +11,7 @@ public class ThemeInfo : ITheme
         Name = "ToSic.Oqt.Themes.ToShineBs5 Theme with Bootstrap 5",
         Version = "1.0.0",
         ThemeSettingsType = Fake,
-        ContainerSettingsType = "Oqtane.Theme.tosic.ContainerSettings, Oqtane.Theme.tosic.Oqtane",
+        ContainerSettingsType = "Oqtane.Theme.ToSic.ContainerSettings, Oqtane.Theme.ToSic.Oqtane",
         PackageName = "ToSic.Oqt.Themes.ToShineBs5"
     };
 }
