12db4c8ed41574c3957b44fe9594025e2b45cab2
ijungleboy <daniel.mettler@2sic.com>
2022-08-25 13:41:19 +0200

    minor clean-up

--- Client/ToShineUtilities.cs
+++ Client/ToShineUtilities.cs
@@ -1,7 +1,3 @@
-using Oqtane.Security;
-using Oqtane.Shared;
-using Oqtane.UI;
-
 namespace ToSic.Oqt.Themes.ToShineBs5.Client;
 
 public static class ToShineUtilities
@@ -10,11 +6,5 @@ public static class ToShineUtilities
     var ns = typeof(ToShineUtilities).Namespace.Replace(".Client", "");
     return "Themes/" + ns + "/Assets/" + filename;
   }
-
-  internal static bool UserIsEditor(PageState pageState)
-      => pageState.User != null && UserSecurity.IsAuthorized(pageState.User, PermissionNames.Edit, pageState.Page.Permissions);
-
-  internal static bool UserIsRegistered(PageState pageState)
-      => pageState.User != null && UserSecurity.IsAuthorized(pageState.User, RoleNames.Registered);
 }
 
---
+++ Client/Utilities/PageStateSecurityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Oqtane.Security;
+using Oqtane.Shared;
+using Oqtane.UI;
+
+namespace ToSic.Oqt.Themes.ToShineBs5.Client.Utilities
+{
+    public static class PageStateSecurityExtensions
+    {
+        internal static bool UserIsEditor(this PageState pageState)
+            => pageState.User != null && UserSecurity.IsAuthorized(pageState.User, PermissionNames.Edit, pageState.Page.Permissions);
+
+        internal static bool UserIsRegistered(this PageState pageState)
+            => pageState.User != null && UserSecurity.IsAuthorized(pageState.User, RoleNames.Registered);
+
+    }
+}
