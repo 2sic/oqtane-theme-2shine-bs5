3650a4a3a09386ea539a1395909705c8ea5f4d13
ijungleboy <daniel.mettler@2sic.com>
2022-08-25 11:26:25 +0200

    clean up login

---
+++ Client/Controls/LoginBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Oqtane;
+
+namespace ToSic.Oqt.Themes.ToShineBs5.Client.Controls
+{
+    public class LoginBase: Oqtane.Themes.Controls.LoginBase
+    {
+        [Inject] private IStringLocalizer<SharedResources> Localizer { get; set; }
+
+        protected bool IsLoggedIn => _isLoggedIn ??= PageState.User is { IsAuthenticated: true };
+        private bool? _isLoggedIn;
+
+        protected string LocalizedLabel => Localizer[IsLoggedIn ? "Logout" : "Login"];
+
+        protected async Task ChangeLogin()
+        {
+            if (IsLoggedIn)
+                await LogoutUser();
+            else
+                LoginUser();
+        }
+    }
+}
