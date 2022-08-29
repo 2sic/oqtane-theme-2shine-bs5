bccc7d2b967579321133314cadd11a299820cf04
ijungleboy <daniel.mettler@2sic.com>
2022-08-29 11:35:45 +0200

    simplifying menus...

--- Client/Menu/ToShineMenuBase.cs
+++ Client/Menu/ToShineMenuBase.cs
@@ -4,12 +4,12 @@ using ToSic.Oqt.Themes.ToShineBs5.Client.Services;
 
 namespace ToSic.Oqt.Themes.ToShineBs5.Client.Menu;
 
-public abstract class MenuListBase: Oqtane.Themes.Controls.MenuBase
+public abstract class ToShineMenuBase: Oqtane.Themes.Controls.MenuBase
 {
     [Parameter]
     [Required]
     public MenuBranch MenuBranch { get; set; }
 
-    public string GetUrl() => GetUrl(MenuBranch.Page);
+    public string GetUrl(MenuBranch branch) => base.GetUrl(branch.Page);
 
 }
