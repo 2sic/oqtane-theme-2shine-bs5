8f246bee5f3567b90003a2422220404ac5b57802
ijungleboy <daniel.mettler@2sic.com>
2022-09-02 10:49:11 +0200

    move menu logic to Cre8ive

---
+++ ToSic.Oqt.Cre8ive.Client/Menu/MenuRootBase.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ToSic.Oqt.Cre8ive.Client.Menu;
+
+/// <summary>
+/// Base class for Razor menus
+/// </summary>
+/// <typeparam name="TPackageSettings"></typeparam>
+public abstract class MenuRootBase<TPackageSettings> : Oqtane.Themes.Controls.MenuBase, IMenuConfig where TPackageSettings : ThemePackageSettingsBase, new()
+{
+    /// <inheritdoc />
+    [Parameter] public string? Id { get; set; }
+    /// <inheritdoc />
+    [Parameter] public string? ConfigName { get; set; }
+    /// <inheritdoc />
+    [Parameter] public List<int>? PageList { get; set; }
+    /// <inheritdoc />
+    [Parameter] public bool? Children { get; set; }
+    /// <inheritdoc />
+    [Parameter] public int? Depth { get; set; }
+    /// <inheritdoc />
+    [Parameter] public bool Debug { get; set; }
+    /// <inheritdoc />
+    [Parameter] public bool? Display { get; set; } = true;
+    /// <inheritdoc />
+    [Parameter] public int? Level { get; set; }
+    /// <inheritdoc />
+    [Parameter] public string? Start { get; set; }
+    /// <inheritdoc />
+    [Parameter] public string? Design { get; set; }
+
+    protected MenuTree? MenuTree { get; private set; }
+
+    [Inject] protected MenuTreeService<TPackageSettings>? MenuTreeService { get; set; }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        MenuTree = MenuTreeService?.GetTree(new MenuConfig(this), PageState, MenuPages.ToList());
+    }
+
+}
